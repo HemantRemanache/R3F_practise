db98c68820204093f1a43ada96de6f914f2efb20
Hemant-Remanacher <hemant.remanache@prototechsolutions.com>
2024-08-15 11:46:57 +0530

    Removing console statements and fixing the alignments

--- src/components/AxisTriadScene.js
+++ src/components/AxisTriadScene.js
@@ -1,12 +1,12 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
 import React, { useRef } from "react";
-import * as THREE from 'three';
-import Short3dScene from './Short3dScene.js';
-export default function AxisTriadScene({ campos,camtarget }) {
+import * as THREE from "three";
+import Short3dScene from "./Short3dScene.js";
+export default function AxisTriadScene({ campos, camtarget }) {
   return (
     <Canvas>
-      <Short3dScene campos={campos} camtarget={camtarget}/>
+      <Short3dScene campos={campos} camtarget={camtarget} />
     </Canvas>
   );
 }
